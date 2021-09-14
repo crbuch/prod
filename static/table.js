f14c77f38318f9142cb6869c7095c903110a1cc7
gabrielavalos <gabby.avalos16@gmail.com>
2021-09-14 14:07:35 -0500

    filter table

--- static/table.js
+++ static/table.js
@@ -10,16 +10,10 @@ d3.json('./static/cumProd.json').then((data) => {
  // CODE FOR CUMULATIVE PRODUCTION TABLE
  // function to import the data from recent.json, saved in this folder from Jupyter Notebook
  function buildTable(tableData) {
-   //clear table...is this necessary?
+   //clear table
    tbody.html("");
-   // read in yesterdays production data for all Hawkwood sites
-   //d3.json("/DATA/recent.json").then(function(data) { //this is giving me an issue !!!!!!
-     // print the array to the console 
-     //console.log(data);
-     // loop thrugh the array
+     // loop thrugh data
      tableData.forEach((well) => {
-       //print each object in the array to the console
-       //console.log(well);
        // for each well add a row to the tbody
        let row = tbody.append("tr");
        // loop through each value to add a cell for each of it
