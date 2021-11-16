1fda790b68edb23c9a23c43a922864a2a7cc0ad1
gabrielavalos <gabby.avalos16@gmail.com>
2021-11-16 15:47:21 -0600

    hid table

--- static/curves.js
+++ static/curves.js
@@ -1,14 +1,14 @@
-async function getOilPrice() {
-  var token = config.MY_API_TOKEN;
-  console.log(token);
-  let response = await fetch('https://commodities-api.com/api/latest?access_key='+token+'&base=USD&symbols=WTIOIL');
-  let data = await response.json()
-  console.log(data);
-  return data;
-}
-getOilPrice().then(data=> document.getElementById("WTIOIL").innerHTML = "WTI: $" + (1/data.data.rates.WTIOIL).toFixed(2)
+// async function getOilPrice() {
+//   var token = config.MY_API_TOKEN;
+//   console.log(token);
+//   let response = await fetch('https://commodities-api.com/api/latest?access_key='+token+'&base=USD&symbols=WTIOIL');
+//   let data = await response.json()
+//   console.log(data);
+//   return data;
+// }
+// getOilPrice().then(data=> document.getElementById("WTIOIL").innerHTML = "WTI: $" + (1/data.data.rates.WTIOIL).toFixed(2)
 
-);
+// );
 
 // FUNCTON TO CREATE WELL OPTIONS DROP DOWN
 function createDropdownOptions() {
