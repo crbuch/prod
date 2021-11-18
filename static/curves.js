5ea543235a7d9009f33ed3a3d6704e39a99d76f3
gabrielavalos <gabby.avalos16@gmail.com>
2021-11-18 11:33:36 -0600

    condensed table()

--- static/curves.js
+++ static/curves.js
@@ -14,13 +14,12 @@ getOilPrice().then(data=> document.getElementById("WTIOIL").innerHTML = "WTI: $"
 function createDropdownOptions() {
   var partnerSelector = d3.select("#siteSelection"); //SELECT <select> WHERE PARTNER NAMES WILL APPEAR
   d3.json("./static/allProductionData.json").then((allData) => { //READ IN JSON FILE COINTAING ALL PARTNER'S NAMES
-    //console.log(allData[0])
     repeatedWells = [] //EMPTY ARRAY TO CONTAIN ALL PARTNER'S NAME (REPEATED)
     allData.forEach((row) => { //LOOP THROUGH NET_INTEREST FILE
     repeatedWells.push(row[0]) //PUSH ALL PARTNER'S NAME TO LIST 
   });
-  wells = [...new Set(repeatedWells)].sort()
-  wells.forEach((well) => {
+  wells = [...new Set(repeatedWells)].sort() //CREATE A UNIQUE LIST OF ALL WELLS AND SORT
+  wells.forEach((well) => { //FOR EACH OF THE UNIQUE WELLS, CREATE AN OPTION FOR THE DROP DOWN
     partnerSelector
     .append('option')
     .text(well)
@@ -32,15 +31,17 @@ function createDropdownOptions() {
 createDropdownOptions();
 
 // LISTENER FOR CHANGE ON DROP DOWN MENU
-d3.select("#siteSelection").on('change', function() {Curve(d=0,t='linear');}); // this is the listener causing issues somehow
+d3.select("#siteSelection").on('change', function() {Curve(d=0,t='linear');}); 
 
 //FUNCTION TO POPULATE CURVE BASED ON ANALYZE CLICK
 function ClickedFromAnalyze(d,t)
-{var clickedFromAnalyzed = sessionStorage.getItem("siteSelection");
+{
+  var clickedFromAnalyzed = sessionStorage.getItem("siteSelection");
   console.log(clickedFromAnalyzed);
   if(clickedFromAnalyzed == null)
+
   {
-    console.log("hello");
+    //console.log("hello");
   }
   else
   {
@@ -236,7 +237,9 @@ if(pumpingWell["Well Name"].includes(selectedOption)){
 });
 }
 })
-};
+
+}; //END OF ELSE STATEMENT
+
 }
 console.log(sessionStorage.getItem("siteSelection"))
 // LISTENER FOR PAGE TO LOAD TO CREATE CURVE FROM ANALYZE TABLE
@@ -250,11 +253,9 @@ function Curve(d,t){
     var dropdownMenu = d3.select("#siteSelection").node();
     var selectedOption = dropdownMenu.value;
   
-  
     document.getElementById("wellName").innerHTML =  selectedOption //DISPLAY WELL'S NAME
     //HIDE PUMPING INFO  SINCE THEY WILL BE SHOWING FROM PREVIOUS SELECTION
     var previousPumpInfo = document.getElementById("pumpInfo");
-  
     previousPumpInfo.style.display = "none"
   
     var previousNotPumpingInfo = document.getElementById("notPumpingInfo")
@@ -447,223 +448,24 @@ function Curve(d,t){
   });    
   }
   })
-
-  //}
-//   else{
-//     selectedOption = clickedFromAnalyzed;
-//   //var dropdownMenu = d3.select("#siteSelection").node(); THIS SHOULD REPLACE  selectedOption = clickedFromAnalyzed; - ROW BEFORE 
-//   //selectedOption = dropdownMenu.value;
-  
-//   document.getElementById("wellName").innerHTML =  selectedOption //DISPLAY WELL'S NAME
-//   //HIDE PUMPING INFO  SINCE THEY WILL BE SHOWING FROM PREVIOUS SELECTION
-//   var previousPumpInfo = document.getElementById("pumpInfo");
-
-//   previousPumpInfo.style.display = "none"
-
-//   var previousNotPumpingInfo = document.getElementById("notPumpingInfo")
-//   previousNotPumpingInfo.style.display = "none"
-
-//   // "CLEANING" PUMP INFO TEXT FOR NEXT SELECTION
-//   document.getElementById("c").innerHTML =  "";
-//   document.getElementById("SPM").innerHTML =  "";
-//   document.getElementById("DHSL").innerHTML =  "";
-//   document.getElementById("ideal").innerHTML =  "";
-//   document.getElementById("pumpEff").innerHTML =  "";
-//   document.getElementById("pumpDepth").innerHTML =  "";
-//   document.getElementById("GFLAP").innerHTML =  "";
-//   document.getElementById("Inc").innerHTML =  "";
-//   document.getElementById("notPumping").innerHTML =  "";
-
-//   console.log(selectedOption);
-  
-//   d3.json("./static/allProductionData.json").then((data) =>{ 
-//     var site_oil = [];
-//     var site_gas = [];
-//     var site_water = [];
-//     var site_date = [];
-//     var comments = [];
-//     var movingAverage = [];
-
-//     data.forEach((site) => {if(site[0] === selectedOption){
-//      site_date.push(site[9]);
-//       site_oil.push(site[2]);
-//       site_gas.push(site[3]);
-//       site_water.push(site[4]);
-//       comments.push(site[7]);
-//       movingAverage.push(site[8])
-//     };
-//   });
-  
-//   if (d > 0){
-//     var site_date = site_date.slice(0,d);
-//     var site_oil = site_oil.slice(0,d);
-//     var site_gas = site_gas.slice(0,d);
-//     var site_water = site_water.slice(0,d);
-//     var comments = comments.slice(0,d);
-//     var movingAverage = movingAverage.slice(0,d);
-//   }
-
-//   var dataOil = {
-//     x: site_date,
-//     y: site_oil,
-//     text: comments,
-//     name: "Oil",
-//     line:
-//     {color: "green"}
-//   };
-
-//   var dataMoving = {
-//     x: site_date,
-//     y: movingAverage,
-//     type: "line",
-//     name: "7 Day Avg",
-//     line:
-//     {dash: "dot"}
-//   }; 
-
-//   var data = [dataOil, dataMoving];
-
-//   var layoutOil = {
-//     title: "Oil (BOPD) vs Time",
-//     yaxis: {
-//       type: t,
-//       rangemode: 'tozero'
-//       // autorange: true
-//     },
-//     legend: {
-//       x: 1,
-//       xanchor: 'right',
-//       y: 1.2
-//     }
-//   };
-
-//   var config = { modeBarButtonsToRemove: ['sendDataToCloud', 'autoScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian', 'lasso2d', 'select2d', 'zoom2d', 'zoomIn2d', 'zoomOut2d', 'toggleSpikelines'], displaylogo: false, responsive: true }; 
-
-//   Plotly.newPlot("oilDeclineCurve", data, layoutOil, config);
-  
-//   var dataGas = [{
-//     x: site_date,
-//     y: site_gas,
-//     text: comments,
-//     line:
-//     {color: "red"}
-//   }];
-//   var layoutGas = {
-//     title: "Gas (MCFD) vs Time",
-//     yaxis: {
-//       type: t,
-//       rangemode: 'tozero'
-//     }
-//   };
-
-//   var config = { modeBarButtonsToRemove: ['sendDataToCloud', 'autoScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian', 'lasso2d', 'select2d', 'zoom2d', 'zoomIn2d', 'zoomOut2d', 'toggleSpikelines'], displaylogo: false, responsive: true };
-
-//   Plotly.newPlot("gasDeclineCurve", dataGas, layoutGas, config);
-  
-//   var dataWater = [{
-//     x: site_date,
-//     y: site_water,
-//     text: comments,
-//     type: "line"
-//   }];
-//   var layoutWater = {
-//     title: "Water (BWPD) vs Time",
-//     yaxis: {
-//       type: t,
-//       rangemode: 'tozero'
-//     }
-//   };
-
-//   var config = { modeBarButtonsToRemove: ['sendDataToCloud', 'autoScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian', 'lasso2d', 'select2d', 'zoom2d', 'zoomIn2d', 'zoomOut2d', 'toggleSpikelines'], displaylogo: false, responsive: true };
-
-//   Plotly.newPlot("waterDeclineCurve", dataWater, layoutWater, config);
-
-//   if(d===0 && t==='log'){
-
-//     var showCurves = document.getElementById("curves"); // CURVES IS INITIALLY DISPLAYED AS LINEAR? 
-//     showCurves.style.display = ""
-
-//     var hideLinear = document.getElementById("timeframes");
-//     hideLinear.style.display = "none";
-
-//     var showLog = document.getElementById("timeframesLog");
-//     showLog.style.display = "";
-
-//     var hidetable = document.getElementById("individualTable");
-//     hidetable.style.display = "none"; 
-//   }
-//   else if(d===0 && t==='linear'){
-//     var hideLog = document.getElementById("timeframesLog");
-//     hideLog.style.display = "none";
-
-//     var showLinear = document.getElementById("timeframes");
-//     showLinear.style.display = "";
-
-//     var showCurves = document.getElementById("curves"); // CURVES IS INITIALLY DISPLAYED AS LINEAR?
-//     showCurves.style.display = ""
-
-//     var hidetable = document.getElementById("individualTable");
-//     hidetable.style.display = "none";
-//   }
-// })
-
-// document.getElementById("siteSelection").focus();
-
-// d3.json("./static/cumProd.json").then((cumData) => {
-//   var selectedWellCum = 0;
-//   var selectedWellGasCum = 0;
-//   cumData.forEach(wellCum=> {
-//     if(selectedOption ===  wellCum[0]){
-//       selectedWellCum = wellCum[1];
-//       selectedWellGasCum = wellCum[3]
-//     }
-//   })
-//   document.getElementById("cumCurve").innerHTML = "Cum: "+ selectedWellCum + " MBBLS, " + selectedWellGasCum + " MMCF";
-// })
-
-// d3.json("./static/pumpInfo.json").then((pumpData) => {
-//   var pumpingInfoToShow = {"Well Name": "doesn't exist because it is not pumping"};
-//   pumpData.forEach((pumpingWell) => {
-// if(pumpingWell["Well Name"].includes(selectedOption)){
-//   pumpingInfoToShow = pumpingWell;
-// }
-//   }) // 
-//   if(pumpingInfoToShow["Well Name"].includes(selectedOption) && pumpingInfoToShow["SPM"] !== 0){
-//  //USED jQuery LIBRARY TO TOGGLE THE DISPLAY OF #pumpInfo
-//     $(document).ready(function() {
-//       $("#pumpInfo").toggle();
-//   });
-//         //SHOW HIDDEN BUTTONS
-//         document.getElementById("c").innerHTML =  "C: " + pumpingInfoToShow["C"]
-//         document.getElementById("SPM").innerHTML =  "SPM: " + pumpingInfoToShow["SPM"]
-//         document.getElementById("DHSL").innerHTML =  "DH SL: "+ pumpingInfoToShow["DH SL"]
-//         document.getElementById("ideal").innerHTML =  "Ideal bfpd: " + pumpingInfoToShow["Ideal bfpd"]
-//         document.getElementById("pumpEff").innerHTML =  "Pump Eff: "+ pumpingInfoToShow["Pump Eff"] //multiply by 100
-//         document.getElementById("pumpDepth").innerHTML =  "Pump Depth: " + pumpingInfoToShow["Pump Depth"] 
-//         document.getElementById("GFLAP").innerHTML =  "GFLAP: " + pumpingInfoToShow["GFLAP"]
-//         document.getElementById("Inc").innerHTML =  "Inc: "+ pumpingInfoToShow["Inc"]
-//   }
-//   else {
-//   $(document).ready(function() {
-//     $("#notPumpingInfo").toggle();
-//     $("#notPumping").html("This well is not pumping");
-// });    
-// }
-// })
-
-//   }
- 
 };
 
 function table() {
+  //SELECT <select> TO LATER "GRAB" THE SELECTION MADE AS TEXTS
   dropdownMenu = d3.select("#siteSelection").node();
- clickedFromAnalyzed = sessionStorage.getItem("siteSelection")
- console.log(clickedFromAnalyzed, "hello")
- 
-
- if(clickedFromAnalyzed == null)
-  {console.log(clickedFromAnalyzed);
-      selectedOption = dropdownMenu.value;
+  //DECLARE ITEM SAVED IN STORAGE
+  var clickedFromAnalyzed = sessionStorage.getItem("siteSelection");
+  //DECLARE WHAT WILL BE SAVED AS THE SELECTION
+  var selectedOption = "";
+  //USE TO DETERMINE SELECTION USED TO CREATE TABLE
+ if(clickedFromAnalyzed == null) //IF NOTHING IN STORAGE USE dropdown.value TO CREATE TABLE
+  {
+    selectedOption = dropdownMenu.value; 
+  }
+  else{ //IF STORAGE IS NOT NULL, TEXT IN STORAGE IS USED TO CREATE TABLE
+   selectedOption = clickedFromAnalyzed;
+  }
+  //READ IN FILE WITH DATA FOR TABLE
     d3.json("./static/allProductionData.json").then((data) => {
       tableData = data;
       tbody = d3.select("tbody");
@@ -676,7 +478,6 @@ function table() {
           cell.text(val);
         })
       }})
-  
       // //HIDE CURVES & BUTTONS, SHOW TABLE
       $(document).ready(function() {
         $("#individualTable").toggle();
@@ -687,38 +488,6 @@ function table() {
        hideCurves.style.display = "";
     });
     })
-    
-  }
-  else
-  {
-var selectedOption = clickedFromAnalyzed;
-
-d3.json("./static/allProductionData.json").then((data) => {
-    tableData = data;
-    tbody = d3.select("tbody");
-    tbody.html("");
-    tableData.forEach((well) => 
-    {if(well[0] === selectedOption){
-      let row = tbody.append("tr");
-      Object.values(well.splice(1,7)).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-      })
-    }})
-
-    // //HIDE CURVES & BUTTONS, SHOW TABLE
-    $(document).ready(function() {
-      $("#individualTable").toggle();
-     // $("#curves").toggle();
-      $("#timeframesLog").toggle();
-      $("#timeframes").toggle();
-      var hideCurves = document.getElementById("curves");
-     hideCurves.style.display = "";
-  });
-  })
-}
-
-  
 };
 
 
@@ -736,9 +505,9 @@ d3.select("#Days30Log").on('click', function() {Curve(d=31,t='log');});
 d3.select("#Days180Log").on('click',function() {Curve(d=181,t='log');});
 d3.select("#Days365Log").on('click', function() {Curve(d=366,t='log');});
 
-// LOG LISTENERS FROM ANALYZE
+// LOG LISTENERS FROM ANALYZE //DONT HAVE 
 //d3.select("#logarithmic").on('load', function() {Curve(d=0,t='log');});
-$( "#logarithmic" ).on( "load", function() {ClickedFromAnalyze(d=0,t='log');}  );
+//$( "#logarithmic" ).on( "click", function() {ClickedFromAnalyze(d=0,t='log');}  );
 
 //TABLE LISTENERS //
 d3.select("#table").on('click', function() {table()});
