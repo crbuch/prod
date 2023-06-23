5e476689c378a25a85ed87a09427edc4d81dadac
Matthew Plaisance <mplaisance128@gmail.com>
2023-06-23 16:53:15 -0500

    testing

--- src/js/curves.js
+++ src/js/curves.js
@@ -332,7 +332,6 @@ const table = (coreData) => {
 
 const switchActives = (event) => {
   event.preventDefault();
-  try{document.getElementById("siteSelection").blur();}catch{};
   
   const target = event.target;
   const parent = document.getElementById(target.id).parentNode;
@@ -346,6 +345,17 @@ const switchActives = (event) => {
   const activeTime = document.getElementById("timeframes").querySelectorAll(".active")[0].id.substring(4);//gives the number from the active view id
   console.log('activeTime :>> ', activeTime);
   curve(Number(activeTime) + 1, curveInfo);
+  function ddd () {
+    console.log("in ddd");
+    try{
+      console.log("closing");
+      document.getElementById("siteSelection").blur();
+      console.log("closed..");
+    }catch{
+      console.log("e");
+    };
+  }
+  setTimeout(ddd,500)
 };
 
 
@@ -384,6 +394,9 @@ dh.dropdown(dropdownId);
 select(dropdownId).on("change", () => {
   curve(localStorage.getItem('initTime'), curveInfo);
 });
+document.getElementById(dropdownId).addEventListener('click', () => {
+  console.log("hit dd");
+})
 
 document.getElementById("table").addEventListener('click', () => {
   if (checkActive('table') === true) return;
