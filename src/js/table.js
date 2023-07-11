ef29d3200963c965009177b2deca56b447785072
DanielMPang <114116375+DanielMPang@users.noreply.github.com>
2023-07-11 10:35:30 -0500

    graph changes on curvves & table

--- src/js/table.js
+++ src/js/table.js
@@ -1,6 +1,6 @@
 import { onAuthStateChangedFb } from './auth';
 import { monitorRegion } from './region'
-import { dataCuml,dataCumlET,payout,activeWells,sortData,buildTable,dropdown,filterData,formations, moDataST, dataST, moDataET, dataET } from './data';
+import { dataCuml,dataCumlET,payout,activeWells,sortData,buildTable,dropdown,filterData,formations,moDataST,dataST,moDataET,dataET } from './data';
 import { formatSpecifier, select } from 'd3';
 import { makeLayout, makeTrace } from './layout';
 
@@ -24,21 +24,21 @@ const formatData = () => {
           let rounded = Math.round(pay["% Payout"]*100)
           well.push(rounded);
         }
-        
       });
     });
 
     tableData.forEach(well => {
       well.push(formations[well[0]])
     });
-  }else {
+  }
+
+  else {
     tableData.forEach(well => {
       well.push('')
       well.push(formations[well[0]])
     });
   }
   
-
   //remove archived wells
   return tableData.filter(val => activeWells().has(val[0]));
 };
@@ -57,7 +57,6 @@ const displayPlot = (selected) => {
   var dateMo = dataMonthly.map(el => el[6]);
   const dateDa = dataDaily.map(el => el[1]).reverse();
   const oilDaily = dataDaily.map(el => el[2]).reverse();
-
   const cumlMoOil = oilMo.reduce((acc, val, idx) => (idx === 0 ? acc.concat(val) : acc.concat(val + acc[idx - 1])), []);
   const cumlDaOil = oilDaily.reduce((acc, val, idx) => (idx === 0 ? acc.concat(val) : acc.concat(val + acc[idx - 1])), []);
 
@@ -95,51 +94,63 @@ const displayPlot = (selected) => {
     "green",
     dateDa
   );
+  const traceMoOil = makeTrace(
+    dateMo,
+    oilMo,
+    "Monthly Oil [BO]",
+    "line",
+    "green"
+  );
 
   // Determine the amount of cycles to show for log graphs
   var maxOilMo = Math.max(...oilMo)
   var maxOilDaily = Math.max(...oilDaily)
   var numDigits_mo = Math.floor(Math.log10(maxOilMo))+1;
   var numDigits_da = Math.floor(Math.log10(maxOilDaily))+1;
+  var cycle = [null, numDigits_mo, numDigits_da, numDigits_mo] // # of cycles to show, null for non-log graph
 
-  var cycle = [null, numDigits_mo, numDigits_da] // # of cycles to show, null for non-log graph
-  let min = (selected=='South Texas Total' || selected=='East Texas Total') ? 2 : 0 // Minimum for graph, only '2' for South Texas Total
+  var min = (selected=='South Texas Total' || selected=='East Texas Total') ? 2 : 1 // Minimum for graph, only '2' for South Texas Total
+
+  var x_arrays = [dateMo, cumlMoOil, cumlDaOil, dateMo] // array of arrays being used on x-axis, used to get start & stop of graphs
 
   // Create Different Keys
-  const plotContainers = ['cumlOilCurve', 'moProdVsCumCurve', 'dailyProdVsCumCurve'];
+  const plotContainers = ['cumlOilCurve', 'moProdVsCumCurve', 'dailyProdVsCumCurve', 'moOilCurve'];
   // Traces to Use
   let traceArrays = [
     [traceCumOil],
     [traceMoProdVSCum],
-    [traceDailyProdVSCum]
+    [traceDailyProdVSCum],
+    [traceMoOil]
   ];
-
   // Loop and make the plots, contain keys found in plotContainers^^
   plotContainers.forEach((container, i) => {
-    // SETS SCALE TO LOG FOR THE Y-AXIS IF (i == [index in plotContainers])
-    let scale = [1, 2].includes(i) ? "log" : null;
+    let scale = [1, 2, 3].includes(i) ? "log" : null; // SETS SCALE TO LOG FOR THE Y-AXIS IF (i == [index in plotContainers])
+
+    // Manual scale for semi-log plot
     let tickvals = scale == 'log' ? [0,1,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,200000,300000,400000,500000,600000,700000,800000,900000,1000000] : 'auto'
     let ticktext = scale == 'log' ? [0,1,10,'','','','','','','','',100,'','','','','','','','',1000,'','','','','','','','',10000,'','','','','','','','',100000,'','','','','','','','',1000000]: 'auto'
     
+    // Plotly Layout
     var layout = {
-      title: ['Time vs Cumulative Oil',  'Cumulative Oil vs Monthly Oil Production', 'Cumulative Oil vs Daily Oil Production'][i],
+      title: ['Time vs Cumulative Oil',  'Cumulative Oil vs Monthly Oil Production', 'Cumulative Oil vs Daily Oil Production', 'Monthly Oil vs Time (BOPM)'][i],
       xaxis: {
-        autorange: true,
+        range: [x_arrays[i][0],x_arrays[i][x_arrays[i].length-1]], // for each plot, range is first index to last index of array that is being used
         showline: scale == 'log' ? true : false,
         gridcolor: 'darkgray',
       },
       yaxis: {
-        range: [min,cycle[i]],
-        // autorange: true,
+        range: scale == 'log' ? [min,cycle[i]] : [0,cumlMoOil[cumlMoOil.length-1]+500], // if log - 1st case, if not log - 2nd case
         type: scale,
         tickvals: tickvals,
         ticktext: ticktext,
+        showline: true,
         gridcolor: 'darkgray',
       },
     }
-    Plotly.newPlot(container, traceArrays[i], layout);
+    Plotly.newPlot(container, traceArrays[i], layout); // Create plot
   });
 }
+
 
 //main
 const tableData = formatData();
